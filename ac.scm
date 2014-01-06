a056b2206b9121340589c0fe8cde0d69f64ff683
Kartik K. Agaram <vc@akkartik.com>
2014-01-06 12:21:01 -0800

    .

--- ac.scm
+++ ac.scm
@@ -329,6 +329,7 @@
     ((equal? params var)  arglist)
     ((not (pair? params))   #f)
     ((assoc (car params) keyword-alist)  (get-arg var (cdr params) arglist keyword-alist))
+    ; destructured optional params
     ((equal? (car params) '?)  (get-arg var (vars-in-optional-paramlist (cdr params))
                                         arglist keyword-alist))
     ((null? arglist)  #f)
